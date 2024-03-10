f175e03078966bed269355d7bcb87fd447b7b12b
cquesson <cquesson@orange.fr>
2024-03-10 15:55:52 +0000

    test 2 blank line

--- file_test.py
+++ file_test.py
@@ -1,19 +1,23 @@
 import pytest
 
+
 def test_calc_addition():
     # fonction test 2+4
     output = 2+4
     assert output == 6
+
 
 def test_calc_substraction():
     # fonction test du resultat 2-4
     output = 2-4
     assert output == -2
 
+
 def test_calc_multiply():
     # resultat de 2*4
     output = 2*4
     assert output == 8
+
 
 def test_coucou():
     # return hello
